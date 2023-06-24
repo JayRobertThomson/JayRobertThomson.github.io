3183ed8942ea81915158f69c902a2c480428fe95
Jay Robert Thomson <106902397+JayRobertThomson@users.noreply.github.com>
2023-06-24 23:23:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -11,3 +11,30 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+(function () {
+    // Function to navigate to a page
+    function navigateToPage(url) {
+        window.location.href = url;
+    }
+
+    // Add click event listeners to the control buttons
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        });
+        button.addEventListener("click", function() {
+            var pageURL = button.dataset.page;
+            if (pageURL) {
+                navigateToPage(pageURL);
+            }
+        });
+    });
+
+    // Toggle light mode
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    });
+})();
